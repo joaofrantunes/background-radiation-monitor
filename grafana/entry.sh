71b186ef7d5bcbd44a0de5431638b53ce3e7a2e9
joaofrantunes <joaofrantunes@gmail.com>
2026-09-19 17:33:50 +0100

    Simplify Grafana startup

--- grafana/entry.sh
+++ grafana/entry.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-/usr/src/app/api.sh &
-exec grafana-server -homepath /usr/share/grafana
+#!/bin/sh
+set -eu
+
+exec grafana-server \
+    -homepath /usr/share/grafana \
+    -config /usr/share/grafana/conf/custom.ini
